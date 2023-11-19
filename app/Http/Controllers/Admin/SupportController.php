029520476ca16776a0d3a9e043937114f6c39820
Robério <rroberiosouza@gmail.com>
2023-11-19 19:12:19 -0300

    update: Modificado o método index do controller SupportController

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
     //
-    public function index()
+    public function index(Support $support)
     {
-        return view('admin/supports/index');
+        $supports = Support::all();
+        return view('admin/supports/index', compact('supports'));
     }
 }

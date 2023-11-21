b49a406506ea6b95885b4d38f13b01eb61279c01
Robério <rroberiosouza@gmail.com>
2023-11-20 21:37:11 -0300

    update: Adicionado rota de editação e exclusão.

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,13 @@ use App\Http\Controllers\Admin\SupportController;
 Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 Route::get('/supports/detail/{id}', [SupportController::class, 'detail'])->name('supports.detail');
+Route::get('/supports/show/update/{id}', [SupportController::class, 'showUpdate'])->name('supports.showUpdate');
+
+Route::put('/supports/update/{id}', [SupportController::class, 'update'])->name('supports.update');
 
 Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+
+Route::delete('/supports/{id}', [SupportController::class, 'delete'])->name('supports.delete');
 
 Route::get('/', function () {
     return view('welcome');

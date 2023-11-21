61fd40a4737baecde4671a9baf19cf2bed4468db
Robério <rroberiosouza@gmail.com>
2023-11-21 20:35:00 -0300

    update: Adicionado os erros de validação na página.

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,7 +1,13 @@
+@if($errors->any())
+    @foreach($errors->all() as $error)
+        {{ $error }}
+    @endforeach
+@endif
+
 <h1>Nova Dúvida</h1>
 <form action="{{ route('supports.store') }}" method="POST">
     @csrf()
-    <input type="text" placeholder="Assunto" name="subject" />
-    <textarea name="body" cols="30" rows="5" placeholder="Descrição"></textarea>
+    <input type="text" placeholder="Assunto" name="subject" value="{{ old('subject') }}"/>
+    <textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea>
     <button type="submit">Enviar</button>
 </form>

68eeb691c5fce24ddce25de39c3310dce12f69a1
Robério <rroberiosouza@gmail.com>
2023-11-20 21:34:57 -0300

    update: Adicionado metodos 'showUpdate', 'update' e 'delete'.

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -30,11 +30,47 @@ class SupportController extends Controller
     }
 
     public function detail(string|int $id)
-    {
-        if (!$support = Support::find($id)){
+    {        
+        if (!$support = Support::find($id))
+        {
             return back();
         }
 
         return view('admin/supports/detail', compact('support'));
+    }
+
+    public function showUpdate(string|int $id)
+    {
+        if (!$support = Support::find($id))
+        {
+            return back();
+        }
+
+        return view('admin/supports/update', compact('support'));
+    }
+
+    public function update(string|int $id, Support $support, Request $request)
+    {  
+        if (!$support = Support::find($id))
+        {
+            return back();
+        }
+            
+        $support::find($id);
+        $support->update($request->only([
+            'subject', 'body'
+        ]));
+
+        return redirect()->route('supports.index');
+    }
+
+    public function delete(string|int $id, Support $support)
+    {
+        if (!$support = Support::find($id))
+        {
+            return back();
+        }
+        $support->delete();
+        return redirect()->route('supports.index');
     }
 }

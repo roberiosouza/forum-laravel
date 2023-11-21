20f57268eee59133c7257747c6dc02340a6327e7
Robério <rroberiosouza@gmail.com>
2023-11-21 20:36:14 -0300

    update: Adicionado aos métodos store e update as validações.

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
 use Illuminate\Http\Request;
 
@@ -20,9 +21,9 @@ class SupportController extends Controller
         return view('admin/supports/create');
     }
     
-    public function store(Request $request, Support $support)
+    public function store(StoreUpdateSupport $request, Support $support)
     {
-        $data = $request->all();
+        $data = $request->validated();
         $data['status'] = 'a';
 
         $support->create($data);
@@ -49,7 +50,7 @@ class SupportController extends Controller
         return view('admin/supports/update', compact('support'));
     }
 
-    public function update(string|int $id, Support $support, Request $request)
+    public function update(string|int $id, Support $support, StoreUpdateSupport $request)
     {  
         if (!$support = Support::find($id))
         {
@@ -57,9 +58,7 @@ class SupportController extends Controller
         }
             
         $support::find($id);
-        $support->update($request->only([
-            'subject', 'body'
-        ]));
+        $support->update($request->validated());
 
         return redirect()->route('supports.index');
     }

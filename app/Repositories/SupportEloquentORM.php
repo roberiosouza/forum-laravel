b10307157e55a8e08ffd0ee6ffef03faa5fbd73a
Robério <rroberiosouza@gmail.com>
2023-11-22 22:52:13 -0300

    add: criado classe SupportEloquentORM

---
+++ app/Repositories/SupportEloquentORM.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Repositories;
+
+use App\DTO\{CreateSupportDTO, UpdateSupportDTO};
+use App\Models\Support;
+use stdClass;
+
+class SupportEloquentORM implements SupportRepositoryInterface
+{
+    public function __construct(
+        protected Support $model
+    ){}
+
+    public function getAll(string $filter = null): array
+    {
+        return $this->model
+                    ->where(
+                        function ($query) use ($filter) {
+                            if ($filter)
+                            {
+                                $query->where('subject', $filter)
+                                ->orWhere('body','LIKE',"%{$filter}%");
+                            }                        
+                        })
+                        ->get()
+                        ->toArray();
+    }
+
+    public function findOne(string $id): stdClass|null
+    {
+        $support = $this->model->find($id);
+        if (!$support)
+        {
+            return null;
+        }
+
+        return (object) $support->toArray();
+    }
+
+    public function delete(string $id): void
+    {
+        $this->model->findOrFail($id)->delete();
+    }
+
+    public function new(CreateSupportDTO $dto): stdClass
+    {
+        $support = $this->model->create(
+            (array) $dto
+        );
+
+        return (object) $support->toArray();
+    }
+
+    public function update(UpdateSupportDTO $dto): stdClass|null
+    {
+        if(!$support = $this->model->find($dto->id))
+        {
+            return null;
+        };
+
+        $support->update(
+            (array) $dto
+        );
+
+        return (object) $support->toArray();
+    }
+}

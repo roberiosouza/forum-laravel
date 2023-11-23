5e7386945cfe71c3c3e4fe053b9428fcc3e385e0
Robério <rroberiosouza@gmail.com>
2023-11-22 22:51:17 -0300

    update: adicionado um bind entre as classes SupportRepositoryInterface e SupportEloquentORM

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Repositories\{SupportRepositoryInterface, SupportEloquentORM};
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,6 +13,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->bind(SupportRepositoryInterface::class, SupportEloquentORM::class);
     }
 
     /**

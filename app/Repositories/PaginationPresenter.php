3d78e3071d2b9edc22afc67a52bbc973caa68bd1
Robério <rroberiosouza@gmail.com>
2023-11-28 21:45:40 -0300

    add: Criado presenter de paginação de Support

---
+++ app/Repositories/PaginationPresenter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+use stdClass;
+
+class PaginationPresenter implements PaginationInterface
+{
+
+    /**
+     * @var stdClass[]
+     */
+    private $items;
+
+    public function __construct(
+        protected LengthAwarePaginator $paginator
+    ){
+        $this->items = $this->resolveItems($this->paginator->items());
+    }
+
+    /**
+     * @return stcClass[]
+     */
+
+     public function items(): array
+     {
+        return $this->items;
+     }
+
+     public function total(): int
+     {
+        return $this->paginator->total() ?? 0;
+     }
+
+     public function isFirstPage(): bool
+     {
+        return $this->paginator->orFirstPage();
+     }
+
+     public function isLastPage(): bool
+     {
+        return $this->paginator->currentPage() === $this->paginator->lastPage();
+     }
+
+     public function currentPage(): int    
+     {
+        return $this->paginator->currentPage() ?? 0;
+     }
+
+     public function getNumberNextPage(): int
+     {
+        return $this->paginator->currentPage() + 1;
+     }
+
+     public function getNumberPreviusPage(): int
+     {
+        return $this->paginator->currentPage() - 1; 
+     }
+
+     private function resolveItems(array $items): array
+     {  
+        $response = [];
+        foreach($items as $item)
+        {
+            $stdClassObject = new stdClass;
+            foreach($item->toArray() as $key => $value)
+            {
+                $stdClassObject->{$key} = $value;
+            }
+            array_push($response, $stdClassObject);
+        }
+        return $response;
+     }
+}
